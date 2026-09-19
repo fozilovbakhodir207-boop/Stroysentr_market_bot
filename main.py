28ec917e99407cff6c9c6f3c0ff856f1d1663974
fozilovbakhodir207-boop <fozilovbakhodir207@gmail.com>
2026-09-19 09:07:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.INFO)
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8599909804:AAGrZoiDTW-dxkoOgyKCbGNBR841TAcchp4")
 ADMIN_ID = os.getenv("ADMIN_ID", "6986848905")
 GROUP_ID = os.getenv("GROUP_ID", "-1004434264658")
-WEB_APP_URL = os.getenv("WEB_APP_URL", "https://stroysentr-market-bot.onrender.com")
+WEB_APP_URL = os.getenv("WEB_APP_URL", "https://fozilovbakhodir207-boop.github.io/Stroysentr_market_bot/")
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
